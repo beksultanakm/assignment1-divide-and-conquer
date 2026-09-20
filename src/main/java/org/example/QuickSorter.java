9f773e9e802ad97a9d07ab0ac72c215958914679
Beksultan <beksultanakm@gmail.com>
2026-09-20 18:27:50 +0600

    beka

--- src/main/java/org/example/QuickSorter.java
+++ src/main/java/org/example/QuickSorter.java
@@ -7,44 +7,28 @@ public class QuickSorter {
     private final Random random = new Random();
 
     public long comparisons;
-    public long swaps;
     public int maxDepth;
 
     public void sort(int[] a) {
         comparisons = 0;
-        swaps = 0;
         maxDepth = 0;
-
         if (a == null || a.length < 2) return;
-
         quickSort(a, 0, a.length - 1, 1);
     }
 
     private void quickSort(int[] a, int left, int right, int depth) {
-        if (depth > maxDepth) maxDepth = depth;
+        maxDepth = Math.max(maxDepth, depth);
 
         while (left < right) {
-            int pivotIndex = left + random.nextInt(right - left + 1);
-            int pivot = a[pivotIndex];
+            int pivot = a[left + random.nextInt(right - left + 1)];
 
-            int lt = left;
-            int gt = right;
-            int i = left;
-
+            int lt = left, i = left, gt = right;
             while (i <= gt) {
                 comparisons++;
-                if (a[i] < pivot) {
-                    swap(a, lt, i);
-                    lt++;
-                    i++;
-                } else if (a[i] > pivot) {
-                    swap(a, i, gt);
-                    gt--;
-                } else {
-                    i++;
-                }
+                if (a[i] < pivot) swap(a, lt++, i++);
+                else if (a[i] > pivot) swap(a, i, gt--);
+                else i++;
             }
-
 
             if (lt - left < right - gt) {
                 quickSort(a, left, lt - 1, depth + 1);
@@ -57,10 +41,8 @@ public class QuickSorter {
     }
 
     private void swap(int[] a, int i, int j) {
-        if (i == j) return;
         int tmp = a[i];
         a[i] = a[j];
         a[j] = tmp;
-        swaps++;
     }
 }
