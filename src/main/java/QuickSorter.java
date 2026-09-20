39a5f35c175687a53a4ad9086c26092904f44946
Beksultan <beksultanakm@gmail.com>
2026-09-20 15:58:32 +0600

    beka

--- src/main/java/QuickSorter.java
+++ src/main/java/QuickSorter.java
@@ -20,3 +20,45 @@ public class QuickSorter {
 
     private void quickSort(int[] a, int left, int right, int depth) {
         if (depth > maxDepth) maxDepth = depth;
+
+        while (left < right) {
+            int pivotIndex = left + random.nextInt(right - left + 1);
+            int pivot = a[pivotIndex];
+
+            int lt = left;
+            int gt = right;
+            int i = left;
+
+            while (i <= gt) {
+                comparisons++;
+                if (a[i] < pivot) {
+                    swap(a, lt, i);
+                    lt++;
+                    i++;
+                } else if (a[i] > pivot) {
+                    swap(a, i, gt);
+                    gt--;
+                } else {
+                    i++;
+                }
+            }
+
+
+            if (lt - left < right - gt) {
+                quickSort(a, left, lt - 1, depth + 1);
+                left = gt + 1;
+            } else {
+                quickSort(a, gt + 1, right, depth + 1);
+                right = lt - 1;
+            }
+        }
+    }
+
+    private void swap(int[] a, int i, int j) {
+        if (i == j) return;
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+        swaps++;
+    }
+}
